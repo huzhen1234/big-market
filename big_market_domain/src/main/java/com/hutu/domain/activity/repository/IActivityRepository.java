4a4d6c28797851a3b33148eba16b120e204aa8c6
huzhen <2027280373@qq.com>
2026-03-12 22:12:13 +0800

    抽奖活动实体的domain层

---
+++ big_market_domain/src/main/java/com/hutu/domain/activity/repository/IActivityRepository.java
@@ -0,0 +1,20 @@
+package com.hutu.domain.activity.repository;
+
+import com.hutu.domain.activity.model.entity.ActivityCountEntity;
+import com.hutu.domain.activity.model.entity.ActivityEntity;
+import com.hutu.domain.activity.model.entity.ActivitySkuEntity;
+
+/**
+ * @description 活动仓储接口
+ */
+public interface IActivityRepository {
+
+    ActivitySkuEntity queryActivitySku(Long sku);
+
+    ActivityEntity queryRaffleActivityByActivityId(Long activityId);
+
+    ActivityCountEntity queryRaffleActivityCountByActivityCountId(Long activityCountId);
+
+    void doSaveOrder(CreateOrderAggregate createOrderAggregate);
+
+}
